70cb31c12e87399aee5099a007b46245d3a880b8
Szabolcs Sztányi <sax155@icloud.com>
2015-07-22 00:28:36 +0200

    created new viewController for favorite concerts

---
+++ SoundLove/SoundLove/viewcontrollers/FavoriteConcertViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteConcertViewController.h
+//  SoundLove
+//
+//  Created by Sztanyi Szabolcs on 20/07/15.
+//  Copyright (c) 2015 Zappdesigntemplates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteConcertViewController : UIViewController
+
+@end
--- SoundLove/SoundLove/viewcontrollers/FavoriteConcertViewController.m
+++ SoundLove/SoundLove/viewcontrollers/FavoriteConcertViewController.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  FavoriteConcertViewController.m
 //  SoundLove
 //
 //  Created by Sztanyi Szabolcs on 20/07/15.
 //  Copyright (c) 2015 Zappdesigntemplates. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "FavoriteConcertViewController.h"
 
-@interface ViewController : UIViewController
-
+@implementation FavoriteConcertViewController
 
 @end
-
