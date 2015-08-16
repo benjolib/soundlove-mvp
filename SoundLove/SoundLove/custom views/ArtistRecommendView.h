d63e1cf65d0930ccf36d337cd181e3064655a3c6
Szabolcs Sztányi <sax155@icloud.com>
2015-08-16 20:53:55 +0200

    added recommended view

--- SoundLove/SoundLove/custom views/ArtistRecommendView.h
+++ SoundLove/SoundLove/custom views/ArtistRecommendView.h
@@ -8,6 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
+@class ArtistModel;
+
+@protocol ArtistRecommendViewDelegate <NSObject>
+
+- (void)artistRecommendViewFadeOutSelected:(ArtistModel*)artistModel;
+- (void)artistRecommendViewFadeInSelected:(ArtistModel*)artistModel;
+
+@end
+
 @interface ArtistRecommendView : UIView
+
+@property (nonatomic, weak) id <ArtistRecommendViewDelegate> delegate;
+
+@property (nonatomic, weak) IBOutlet UILabel *fadeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *artistNameLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *fadeController;
+@property (nonatomic, weak) IBOutlet UIImageView *fadeControlBackgroundView;
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *fadeControllerCenterXConstraint;
+
+// circle view
+@property (nonatomic, weak) IBOutlet UIView *circleBackgroundView;
+@property (nonatomic, weak) IBOutlet UIView *imageWrapperView;
+@property (nonatomic, weak) IBOutlet UIImageView *artistImageView;
+@property (nonatomic, weak) IBOutlet UIView *middleCircleView;
+@property (nonatomic, weak) IBOutlet UIView *middleSmallCircleView;
+
+- (void)showViewWithArtist:(ArtistModel*)artist;
 
 @end
--- SoundLove/SoundLove/custom views/ArtistRecommendView.m
+++ SoundLove/SoundLove/custom views/ArtistRecommendView.m
@@ -7,7 +7,104 @@
 //
 
 #import "ArtistRecommendView.h"
+#import "ArtistModel.h"
+#import "ImageDownloader.h"
+
+@interface ArtistRecommendView ()
+@property (nonatomic, strong) ArtistModel *artistModel;
+@property (nonatomic, strong) ImageDownloader *imageDownloader;
+@end
 
 @implementation ArtistRecommendView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSArray *bundle = [[NSBundle mainBundle] loadNibNamed:@"ArtistRecommendView" owner:nil options:nil];
+        for (id object in bundle) {
+            if ([object isKindOfClass:[ArtistRecommendView class]]) {
+                self = object;
+                break;
+            }
+        }
+
+        [self setupFadeController];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    self.circleBackgroundView.layer.cornerRadius = CGRectGetHeight(self.circleBackgroundView.frame)/2;
+    self.imageWrapperView.layer.cornerRadius = CGRectGetHeight(self.imageWrapperView.frame)/2;
+    self.middleCircleView.layer.cornerRadius = CGRectGetHeight(self.middleCircleView.frame)/2;
+    self.middleSmallCircleView.layer.cornerRadius = CGRectGetHeight(self.middleSmallCircleView.frame)/2;
+}
+
+- (void)showViewWithArtist:(ArtistModel*)artist
+{
+    self.artistModel = artist;
+    self.artistNameLabel.text = self.artistModel.name;
+
+    self.imageDownloader = [[ImageDownloader alloc] init];
+    [self.imageDownloader startDownloadingImage:self.artistModel.imageURL completionBlock:^(UIImage *image) {
+        if (image) {
+            self.artistImageView.image = image;
+            self.artistModel.image = image;
+        }
+    }];
+}
+
+- (void)setupFadeController
+{
+    UIPanGestureRecognizer *panrecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(fadeControllerPanned:)];
+    [self.fadeController addGestureRecognizer:panrecognizer];
+}
+
+- (void)fadeControllerPanned:(UIPanGestureRecognizer*)panRecognizer
+{
+    switch (panRecognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            break;
+        case UIGestureRecognizerStateChanged:
+        {
+            CGPoint locationPoint = [panRecognizer locationInView:self.fadeControlBackgroundView];
+            if (locationPoint.x <= 10.0) {
+                return;
+            }
+            if (locationPoint.x >= CGRectGetWidth(self.fadeControlBackgroundView.frame)-10.0) {
+                return;
+            }
+            self.fadeControllerCenterXConstraint.constant = [panRecognizer translationInView:self.fadeController].x;
+        }
+            break;
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateEnded:
+        {
+            if (self.fadeControllerCenterXConstraint.constant < 30.0) {
+                if ([self.delegate respondsToSelector:@selector(artistRecommendViewFadeOutSelected:)]) {
+                    [self.delegate artistRecommendViewFadeOutSelected:self.artistModel];
+                }
+            } else if (self.fadeControllerCenterXConstraint.constant >= CGRectGetWidth(self.fadeControlBackgroundView.frame)-30.0) {
+                if ([self.delegate respondsToSelector:@selector(artistRecommendViewFadeInSelected:)]) {
+                    [self.delegate artistRecommendViewFadeInSelected:self.artistModel];
+                }
+            }
+
+            self.fadeControllerCenterXConstraint.constant = 0.0;
+            [UIView animateWithDuration:0.3 animations:^{
+                [self layoutIfNeeded];
+            }];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+
 
 @end
